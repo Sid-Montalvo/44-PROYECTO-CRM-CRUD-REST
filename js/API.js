876eee3329e3be670317b16cd57fdab1c6902b09
Sid Montalvo <pizzza.planeta@gmail.com>
2022-11-15 00:22:46 -0600

    Se agrega el proyecto

---
+++ js/API.js
@@ -0,0 +1,35 @@
+const url = 'http://localhost:4000/clientes'
+
+
+// Cuando se crea un nuevo cliente
+export const nuevoCliente = async cliente => {
+
+
+    try {
+        await fetch (url, {
+            method: 'POST',
+            body: JSON.stringify( cliente ),
+            headers: {
+                'Content-Type': 'application/json'
+            } 
+        });
+        window.location.href = 'index.html';
+    } catch (error) {
+
+    console.log(cliente);
+        
+    }
+}
+
+// Obtiene todos los clientes
+export const obtenerClientes = async () => {
+
+
+    try {
+        const resultado = await fetch (url);
+        const clientes = await resultado.json();
+        return clientes;
+    } catch (error) {
+        console.log(error);
+    }
+} 
---
+++ js/app.js
@@ -0,0 +1,15 @@
+import { obtenerClientes } from "./API.js";
+
+(function () {
+    const listado = document.querySelector('#listado-clientes');
+
+    document.addEventListener('DOMContentLoaded', mostrarClientes);
+
+    function mostrarClientes() {
+        const clientes = obtenerClientes();
+
+        console.log(clientes);
+    }
+
+    
+}) ();
